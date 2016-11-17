83b6d1f03b649e9d31843db87f5b2219150037d6
richjohnsonxyz <richjohnsonxyz@gmail.com>
2016-11-17 04:59:50 -0800

    Finished - Meets Expectations

--- Project 4 Tic-Tac-Toe/js/T3-UI.js
+++ Project 4 Tic-Tac-Toe/js/T3-UI.js
@@ -1,23 +1,19 @@
 //User Interface - Event Listeners
 "use strict";
+
 //Hide Finish Screen
 $('.screen-win').hide();
+
 //Start Game
 $('.button').on('click', function () {
 	T3.reset();
 	T3.start();
 });
 
-//Mouseover boxes
-$('.box').mouseover(function() {
-	let that = this;
-	T3.hover(that);
-});
-
-//var count = 0;
 //Selection Made
 $('.box').click(function() {
 	let that = this;
 	T3.click(that);
 });
+	
 	
