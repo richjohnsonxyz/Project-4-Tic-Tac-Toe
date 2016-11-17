02e2ba3513809004c97f240b80dd10542b55e9be
richjohnsonxyz <richjohnsonxyz@gmail.com>
2016-11-17 04:59:01 -0800

    Finished Project - Meets Expectations

--- Project 4 Tic-Tac-Toe/js/T3-Game.js
+++ Project 4 Tic-Tac-Toe/js/T3-Game.js
@@ -1,9 +1,12 @@
 var T3 = {
+	//Keep track of the number of filled squares
 	count: 0,
+	//Start Game
 	start: function () {
 		$('#player1').addClass('active');
 		T3.hover();
 	},
+	//Reset the game, clear the board
 	reset: function () {
 		$('.boxes').children().removeClass('box-filled-1 box-filled-2');
 		$('#player2').removeClass('active');
@@ -14,6 +17,7 @@ var T3 = {
 		$('#board').show();	
 		count = 0;
 	},
+	//Change Player Turns
 	active: function (that) {
 		if ($('#player1').hasClass('active')) {
 			$('#player1').removeClass('active');
@@ -26,108 +30,104 @@ var T3 = {
 			T3.hover(that);
 		}
 	},
-	
+	//Show "O" or "X" hover image on blank squares
 	hover: function (that) {
-		if ($('#player1').hasClass('active')) {
-			$('.box').hover(function() {
+		$('.box').hover(function() {
+			if ( $('#player1').hasClass('active') && !$(this).hasClass('box-filled-2')) {
 				$(this).css('backgroundImage', 'url("img/o.svg")');
-				}, function() {
-					$(this).removeAttr('style');
-				}
-			);
-		} else if ($('#player2').hasClass('active')) {
-			$('.box').hover(function() {
+			} else if ( $('#player2').hasClass('active') && !$(this).hasClass('box-filled-1')) {
 				$(this).css('backgroundImage', 'url("img/x.svg")');
-				}, function() {
-					$(this).removeAttr('style');
-				}
-			);
-		}
-		
+			}},	function() {
+				$(this).removeAttr('style');
+			} 
+		);
 	},
-	
+	//Square is filled with either an "O" or an "X"
 	click: function (that) {
-		
-		console.log(count);
+		//The box is already filled - do nothing
 		if ( $(that).hasClass('box-filled-1') || $(that).hasClass('box-filled-2') ) {
-			console.log('1st');
 			return;
 		}
+		//Keep track of the number of filled boxes
+		count += 1;
+		//O's and X's
 		if ($('#player1').hasClass('active')) {
 			$(that).addClass('box-filled-1');
-			T3.win(that);
-			console.log('2nd');
+			T3.active(that);
 		} else {
 			$(that).addClass('box-filled-2');
+			T3.active(that);
+		}
+		//5 boxes or more - check to see if there is a winner
+		if (count >= 5) {
 			T3.win(that);
-			console.log('3rd');
 		}
-		count += 1;
-		//BUG if it's 9 but it wins 
-		if (count === 9) {
-			T3.end(0);
-		}
-		console.log(count);
-	},
-	
-	checker: function() {
-		//Check each item if it has 1 or 2
-		var a = $('.boxes').children();
-		var z = [];
-		$(a).each(function(i) {
-			if ( $(this).hasClass('box-filled-1') || $(this).hasClass('box-filled-2')) {
-				z.push(this);
-				console.log(z);
-			};
-		});
 	},
 	
 	//$('.boxes').children().length = 9 (0,1,2) x (3,4,5) x (6,7,8)
 	win: function(that) {
 		var a = $('.boxes').children();
+		
 		// Check if Player 1 "O" won
 		if ( $(that).hasClass('box-filled-1') ) {
 			if ( $(a[0]).hasClass('box-filled-1') && $(a[1]).hasClass('box-filled-1') && $(a[2]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[3]).hasClass('box-filled-1') && $(a[4]).hasClass('box-filled-1') && $(a[5]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[6]).hasClass('box-filled-1') && $(a[7]).hasClass('box-filled-1') && $(a[8]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[0]).hasClass('box-filled-1') && $(a[3]).hasClass('box-filled-1') && $(a[6]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[1]).hasClass('box-filled-1') && $(a[4]).hasClass('box-filled-1') && $(a[7]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[2]).hasClass('box-filled-1') && $(a[5]).hasClass('box-filled-1') && $(a[8]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[0]).hasClass('box-filled-1') && $(a[4]).hasClass('box-filled-1') && $(a[8]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			} else if ( $(a[2]).hasClass('box-filled-1') && $(a[4]).hasClass('box-filled-1') && $(a[6]).hasClass('box-filled-1') ) {
 				T3.end(1);
+				return;
 			}
 		} 
 		// Check if Player 2 "X" won
 		else {
 			if ( $(a[0]).hasClass('box-filled-2') && $(a[1]).hasClass('box-filled-2') && $(a[2]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[3]).hasClass('box-filled-2') && $(a[4]).hasClass('box-filled-2') && $(a[5]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[6]).hasClass('box-filled-2') && $(a[7]).hasClass('box-filled-2') && $(a[8]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[0]).hasClass('box-filled-2') && $(a[3]).hasClass('box-filled-2') && $(a[6]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[1]).hasClass('box-filled-2') && $(a[4]).hasClass('box-filled-2') && $(a[7]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[2]).hasClass('box-filled-2') && $(a[5]).hasClass('box-filled-2') && $(a[8]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[0]).hasClass('box-filled-2') && $(a[4]).hasClass('box-filled-2') && $(a[8]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			} else if ( $(a[2]).hasClass('box-filled-2') && $(a[4]).hasClass('box-filled-2') && $(a[6]).hasClass('box-filled-2') ) {
 				T3.end(2);
+				return;
 			}
 		}
-				
-		T3.active(that);
-		
+		// IT'S A TIE - If all boxes are filled and there is no winner
+		if (count === 9) {
+			T3.end(0);
+		}	
 	},
 	
 	end: function(finish) {
@@ -154,4 +154,4 @@ var T3 = {
 		}
 	}
 
-}
+};
